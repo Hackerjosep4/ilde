887eabf85b62a773615f398fe0cf99ebc51cf1e7
Josep <hackerjosep4.yt@gmail.com>
2026-02-05 13:41:42 +0100

    [change] can introduce void vals

--- mandelbrot/mandelbrot_zoom.py
+++ mandelbrot/mandelbrot_zoom.py
@@ -10,12 +10,12 @@ while True:
     img = mandelbrot(0.5, 100, inx, inx+dst, iny, iny+dst)
     img.save(f"mdbt_zoom_{inx:.3f}_{iny:.3f}_{dst:.3f}.png")
     img.show()
-    cc = int(input("Tamany de c de la cuadricula (0 per sortir): "))
+    cc = int(input("Tamany de c de la cuadricula (0 per sortir): ") or 1)
     if cc == 0:
         break
-    sx = float(input("Coordenada x de la cuadricula: "))-1
-    sy = float(input("Coordenada y de la cuadricula: "))-1
-    tm = float(input("Cantitat de cuadrats de la cuadricala agafats: "))
+    sx = float(input("Coordenada x de la cuadricula: ") or 0)-1
+    sy = float(input("Coordenada y de la cuadricula: ") or 0)-1
+    tm = float(input("Cantitat de cuadrats de la cuadricala agafats: ") or 1)
     dst = dst / cc * tm
     inx = inx + sx * dst
     iny = iny + sy * dst
